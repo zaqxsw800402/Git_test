59e7634ad465fd129d80b173454d4b0bc5ea5418
IChih <zaqxsw79218@gmail.com>
2021-05-25 14:29:18 +0800

    Create keypoint_classifier.py

---
+++ keypoint_classifier.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import tensorflow as tf
+
+
+class KeyPointClassifier(object):
+    def __init__(
+        self,
+        model_path='model/keypoint_classifier/keypoint_classifier.tflite',
+        num_threads=1,
+        score_th=0.5,
+        invalid_value=0,
+    ):
+        self.interpreter = tf.lite.Interpreter(model_path=model_path,
+                                               num_threads=num_threads)
+
+        self.interpreter.allocate_tensors()
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
+
+        self.score_th = score_th
+        self.invalid_value = invalid_value
+
+    def __call__(
+        self,
+        landmark_list,
+    ):
+        input_details_tensor_index = self.input_details[0]['index']
+        self.interpreter.set_tensor(
+            input_details_tensor_index,
+            np.array([landmark_list], dtype=np.float32))
+        self.interpreter.invoke()
+
+        output_details_tensor_index = self.output_details[0]['index']
+
+        result = self.interpreter.get_tensor(output_details_tensor_index)
+        print(f'result {result}')
+
+        result_index = np.argmax(np.squeeze(result))
+        print(f'result_index {result_index}')
+
+        if np.squeeze(result)[result_index] < self.score_th:
+            result_index = self.invalid_value
+        print(f'output {result_index}')
+
+        return result_index
